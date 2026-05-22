d3140480f4c7c8c6a585267d81d5e78897d509d4
Termux Kullanici <termux@kullanici.com>
2026-05-22 17:42:53 +0300

    Sadece 4 site: HentaiHaven, Hanime, HentaiCity, PimpBunny

--- HentaiCity/build.gradle.kts
+++ HentaiCity/build.gradle.kts
@@ -1,4 +1,5 @@
 // use an integer for version numbers
+<<<<<<< HEAD
 version = 2
 
 
@@ -23,3 +24,32 @@ cloudstream {
 
     iconUrl = "https://www.google.com/s2/favicons?domain=animeidhentai.com&sz=%size%"
 }
+=======
+version = 7
+
+
+cloudstream {
+    // All of these properties are optional, you can safely remove them
+
+    description = ""
+    authors = listOf("Jace")
+
+    /**
+    * Status int as the following:
+    * 0: Down
+    * 1: Ok
+    * 2: Slow
+    * 3: Beta only
+    * */
+    status = 1 // will be 3 if unspecified
+
+    // List of video source types. Users are able to filter for extensions in a given category.
+    // You can find a list of avaliable types here:
+    // https://recloudstream.github.io/cloudstream/html/app/com.lagradost.cloudstream3/-tv-type/index.html
+    tvTypes = listOf("NSFW")
+
+    iconUrl = "https://www.google.com/s2/favicons?domain=hentaihaven.xxx&sz=%size%"
+
+    language = "en"
+}
+>>>>>>> ffe409a (4 site eklendi: HentaiHaven, Hanime, HentaiCity, PimpBunny)
